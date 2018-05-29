20ebf8b2a656ee2d22a607378dd07cecd7ab76fb
Stuart Prescott <s.prescott@unsw.edu.au>
2018-05-29 23:31:23 +1000

    Get version from module

--- setup.py
+++ setup.py
@@ -5,13 +5,15 @@ try:
 except ImportError:
     from distutils.core import setup
 
+import moodletools
+
 config = {
     'description': 'Moodle utilities',
     'author': 'Stuart Prescott',
     'url': 'http://www.complexfluids.net/',
     'download_url': 'http://www.complexfluids.net/',
     'author_email': 's.prescott@unsw.edu.au',
-    'version': '0.1',
+    'version': moodletools.__version__,
     'install_requires': [],
     'packages': ['moodletools'],
     'name': 'moodletools',
